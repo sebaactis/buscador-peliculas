29f36ac51a3314cd21ccdde993a13fe8b1ea0c27
sebastian.actis <sebaactis@gmail.com>
2023-02-09 15:57:30 -0300

    control del formulario

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 
@@ -5,15 +6,45 @@ import { useMovies } from "./hooks/useMovies";
 function App() {
 
   const { movies } = useMovies();
+  const [query, setQuery] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(query)
+  }
+
+  const handleChange = (e) => {
+    const newQuery = e.target.value
+    if (newQuery.startsWith(" ")) return
+    setQuery(e.target.value)
+  } 
+  
+  useEffect(() => {
+
+    if (query === "") {
+      setError("El campo esta vacio")
+      return;
+    }
+
+    if (query.length < 3) {
+      setError("El nombre de la pelicula debe tener mas de 3 caracteres")
+      return;
+    }
+
+    setError(null);
+
+  }, [query])
 
   return (
     <div className="page">
       <header>
         <h2 className="titulo"> Buscador de peliculas </h2>
-        <form>
-          <input placeholder="Advengers, Spiderman..." type="text" />
+        <form onSubmit={handleSubmit}>
+          <input value={query} onChange={handleChange} placeholder="Advengers, Spiderman..." type="text" />
           <button type="submit"> Search </button>
         </form>
+        {error && <p style={{color: "red"}}> {error} </p>}
       </header >
 
       <main >
